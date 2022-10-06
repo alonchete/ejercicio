35005b8e065484243f03da7e24db201e218f4463
alonchete <alonsete.tyr@gmail.om>
2022-10-06 10:56:38 +0200

    formu

---
+++ ingles.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang=”en”>
+
+<head>
+
+    <link rel="stylesheet" href="estilo.css" type="text/css">
+    <meta charset=”UTF-8″ />
+    <title>Verbos</title>
+</head>
+
+<body>
+    <?php
+    $q = 0;
+    do {
+    $q++;
+    }while ($q < 7);
+
+echo $q;
+    ?>
+</body>
+
+</html>
--- registro.php
+++ registro.php
@@ -10,37 +10,52 @@
 
 <body>
     <?php
+
+$nacion = array("Española", "Inglesa", "Francesa","Argentina", "Uruguaya" );
+$aficiones = array("Música","Boxeo","Videojuegos","Bailar");
     
-    if(isset($_REQUEST["sexo"]) and isset($_REQUEST["idiomas"]) and isset($_REQUEST["nombre"])and 
+    if(isset($_REQUEST["sexo"])  and isset($_REQUEST["nombre"])and 
     isset($_REQUEST["apellidos"]) and isset($_REQUEST["nacionalidad"]) and isset($_REQUEST["aficiones"])  ){ 
         
          $sx = $_POST["sexo"];
+         if( isset($_REQUEST["idiomas"]) ){
          $id = $_POST["idiomas"];
+         }else $id = array("Sin especificar ");
          $nm = $_POST["nombre"];
          $ap = $_POST["apellidos"];
          $na = $_POST["nacionalidad"];
          $af = $_POST["aficiones"];
-
+        
         echo "<table>";
         echo "<tr><td>Nombre</td> <td>$nm</td></tr>";
-        echo "<tr><td>Apellidos</td>  <td>$ap</td></tr>";
+        echo "<tr><td>Apellidos</td><td>$ap</td></tr>";
         echo "<tr><td>Sexo</td> <td>$sx</td></tr>";
+       
         echo "<tr><td>Idiomas</td>";
         foreach($id as $valor){
         echo "<td>$valor</td>";
         }
         echo "</tr>";
+        echo "<tr><td>Aficion</td>";
+
+        foreach($af as $valor){
+            echo "<td>$valor</td>";
+            }
+
+        echo "</tr>";
+        echo "<tr><td>Nacionalidad</td> <td>$na</td></tr>";
+
         echo "</table>";
 
 
+
       
-    }else    
-    echo "<form name='formulario' method='post' action='http://localhost/ejercicio/registro.php'>"
-    
-    echo   "<form name='formulario' method='post' action='http://localhost/ejercicio/registro.php'>
+    }else  { echo   "<form name='formulario' method='post' action='http://localhost/ejercicio/registro.php'>
             <h4>Rellene todos los campos, no está registrado</h4>
-             <input type='text' name='nombre'> Nombre</input><br>
-             <input type='text' name='apellidos'> Apellidos</input><br>
+            <label>Nombre</label><br>
+             <input type='text' name='nombre'><br>
+             <label>Apellidos</label><br>
+             <input type='text' name='apellidos'><br>
              <input type='radio' name='sexo' value='Hombre'> hombre</input>
              <input type='radio' name='sexo' value='Mujer'> mujer</input><br>
              <input type='checkbox' name='idiomas[]' value='ingles'> ingles</input>
@@ -49,28 +64,22 @@
              <input type='checkbox' name='idiomas[]' value='Frances'> Frances</input><br>
              <input type='checkbox' name='idiomas[]' value='Portugués'> Portugués</input><br>";
 
-             echo"   <label>Nacionalidad</label>
-                <select name='nacionalidad'>
-                <option value='Española'>Española</option>
-                <option value='Francesa'>Francesa</option>
-                <option value='Inglesa'>Inglesa </option>
-                <option value='Argentina'>Argentina</option>
-                    <option value='Portuguesa'>Portuguesa</option>
-                    </select><br>";
-
+    echo"<label>Nacionalidad</label>
+                <select name='nacionalidad'>" ;
+                    foreach($nacion as $valor){
+                        echo "<option value='$valor'>$valor</option>";
+                        } 
+    echo "</select><br>";
+            
            echo " <label>Aficiones</label><br>
-            <select name='aficiones[]' multiple>
-            <option value='Musica'>Musica</option>
-            <option value='Deporte'>Deporte</option>
-            <option value='Peliculas'>Peliculas </option>
-            <option value='Videojuegos'>Videojuegos</option>
-            <option value='Cantar junto a los peces'>Cantar junto a los peces</option>
-           </select>
-
-
-             <input type='submit' value='Subir'/>
-            ";
-                echo "</form>";
+            <select name='aficiones[]' multiple>";
+            foreach($aficiones as $valor){
+                echo "<option value='$valor'>$valor</option>";
+                } 
+          echo "</select>";
+          echo" <input type='submit' value='Subir'/>";      
+          echo "</form>";
+            }
 
     ?>
 </body>
